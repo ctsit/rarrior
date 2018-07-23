4c738a81c6c9cade9e07e04d7b1bbecb2337a628
Marly Cormar <marlycormar@gmail.com>
2018-07-23 16:28:48 -0400

    Include comments on the script get_latest_warrior_data_file_names that follows roxygen2 format.

--- R/get_latest_warrior_data_file_names.R
+++ R/get_latest_warrior_data_file_names.R
@@ -1,15 +1,16 @@
+#' Gets the latest downloaded files (in terms of date) from the provided
+#' from_directory, as well as the date reflected in the filenames.
+#' If no arguments are provided, the directory is assumed to be the '~/Downloads/' folder.
+#'
+#' @param from_directory A directory where to find the latest data.
+#' @return A list containing of key-value pairs
+#'         returned_list[['data']] to get the data file name,
+#'         returned_list[['metadata']] to get the metadata file name and,
+#'         returned_list[['date']] to get the date associated with the data.
+#' @examples
+#' get_latest_warrior_data_file_names()
+#' get_latest_warrior_data_file_names('/warrior_data_file_directory')
 get_latest_warrior_data_file_names <- function(from_directory='~/Downloads/') {
-  # Gets the latest downloaded files (in terms of date) from the provided
-  # from_directory, as well as the date reflected in the filenames.
-  # If no arguments are provided, the directory is assumed to be the '~/Downloads/' folder.
-  #
-  # Args:
-  #   from_directory = a directory where to find the latest data,
-  # Returns:
-  #   a list containing of key-value pairs
-  #     returned_list[['data']] to get the data file name,
-  #     returned_list[['metadata']] to get the metadata file name and
-  #     returned_list[['date']] to get the date associated with the data.
   patterns <- c("data" = "WARRIOR_DATA_20\\d\\d.*\\.csv",
                 "metadata" = "WARRIOR_DataDictionary_20\\d\\d.*\\.csv",
                 "date" = "\\d{4}-\\d{2}-\\d{2}")
